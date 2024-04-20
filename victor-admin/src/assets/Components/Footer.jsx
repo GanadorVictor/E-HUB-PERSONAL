827cca3f16b9deda92ea5ad08cc7e5816bfefc17
nandabi <valerie.nandabi@student.moringaschool.com>
2024-04-20 19:42:47 +0300

    commit message

--- victor-admin/src/assets/Components/Footer.jsx
+++ victor-admin/src/assets/Components/Footer.jsx
@@ -7,12 +7,16 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-section">
           <h3>About Us</h3>
-          <p>We are one of the leading e-commerce sites in the country so shop with us today</p>
+          <p>Your best online ecommerce store in Kenya and East Africa 
+            Specializing in Information technology & telecommunication 
+            products, general electronics and lifestyle products.
+            Our Philosophy - Whatever You Need, We've Got It.</p>
         </div>
         <div className="footer-section">
           <h3>Contact Us</h3>
           <p>Email: admin@example.com</p>
           <p>Phone: +1234567890</p>
+          <p>ehub(On All Social Media Platforms)</p>
         </div>
         
         
